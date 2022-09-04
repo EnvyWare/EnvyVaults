b36da3cfe3b824ecc4f2cf697832a66372030a3c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2022-09-04 19:56:39 +0100

    feat(forge16): add player attribute

---
+++ forge16/src/main/java/com/envyful/vaults/player/VaultsAttribute.java
@@ -0,0 +1,67 @@
+package com.envyful.vaults.player;
+
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.api.forge.player.attribute.AbstractForgeAttribute;
+import com.envyful.api.forge.player.util.UtilPlayer;
+import com.envyful.api.player.EnvyPlayer;
+import com.envyful.api.player.attribute.PlayerAttribute;
+import com.envyful.api.player.save.attribute.DataDirectory;
+import com.envyful.vaults.EnvyVaults;
+import com.envyful.vaults.config.EnvyVaultsConfig;
+
+import java.util.List;
+import java.util.UUID;
+
+@DataDirectory("config/players/EnvyVaults/")
+public class VaultsAttribute extends AbstractForgeAttribute<EnvyVaults> {
+
+    private List<PlayerVault> vaults;
+    private int allowedVaults = 0;
+
+    public VaultsAttribute(EnvyVaults manager, EnvyPlayer<?> parent) {
+        super(manager, (ForgeEnvyPlayer) parent);
+    }
+
+    public VaultsAttribute(UUID uuid) {
+        super(uuid);
+    }
+
+    public List<PlayerVault> getVaults() {
+        return this.vaults;
+    }
+
+    public boolean canAccess(int vaultId) {
+        return vaultId <= this.allowedVaults;
+    }
+
+    public PlayerVault getVault(int vaultId) {
+        for (PlayerVault vault : this.vaults) {
+            if (vault.getId() == vaultId) {
+                return vault;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void postLoad() {
+        for (EnvyVaultsConfig.VaultGroups vault : this.manager.getConfig().getVaults()) {
+            if (UtilPlayer.hasPermission(this.getParent().getParent(), vault.getPermission())) {
+                if (vault.getVaultNumber() > this.allowedVaults) {
+                    this.allowedVaults = vault.getVaultNumber();
+                }
+            }
+        }
+    }
+}
